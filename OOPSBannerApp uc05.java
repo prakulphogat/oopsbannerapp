e85edfa8d16a1804fbee730fec80530ca31be6ae
PRAKUL PHOGAT <prakulphogat2922@gmail.com>
2026-02-21 15:24:22 +0530

    Add files via upload

---
+++ OOPSBannerApp uc05.java
@@ -0,0 +1,21 @@
+class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+            "   ***        ***      ******        ***** ",
+            " **   **    **   **    **    **     **     ",
+            "**     **  **     **   **     **   **     ",
+            "**     **  **     **   **    **     **  ",
+            "**     **  **     **   ******        ***",
+            "**     **  **     **   **              **  ",
+            "**     **  **     **   **               **     ",
+            " **   **    **    **   **              **       ",
+            "   ***        ***      **         *****   " 
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
